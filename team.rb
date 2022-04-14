d01ad9cddcad68460d8be1b75af42d5826ff77c7
Taitai-Kawakami <tai.zkx@gmail.com>
2022-04-14 09:20:07 +0000

    sard commit

--- team.rb
+++ team.rb
@@ -1,44 +1,38 @@
 class Team
 
- attr_accessor :name, :win, :lose, :draw, :win_rate
- 
+attr_accessor :name, :win, :lose, :draw
+
   def initialize(name, win, lose, draw)
     self.name = name
     self.win = win
     self.lose = lose
     self.draw = draw
-    self.win_rate = 0
   end
-  
+
   def calc_win_rate()
-     self.win_rate = self.win.to_f / (self.win + self.lose)
+    self.win.to_f / (self.win + self.lose)
   end
-  
-  def show_team_result()
-    puts "#{self.name} の2020年の成績は #{self.win}勝 #{self.lose}敗 #{self.draw}分、勝率は#{self.win_rate}です。"
+
+  def show_team_result
+    puts "#{self.name} の2020年の成績は #{self.win}勝 #{self.lose}敗 #{self.draw}分、勝率は#{self.calc_win_rate}です。"
   end
 end
 
 giants = Team.new("Giants", 67, 45, 8)
-giants.calc_win_rate
 giants.show_team_result
 
 tigers = Team.new("Tigers", 60, 53, 7)
-tigers.calc_win_rate
 tigers.show_team_result
 
 dragons = Team.new("Dragons", 60, 55, 5)
-dragons.calc_win_rate
 dragons.show_team_result
 
+
 baystars = Team.new("BayStars", 56, 58, 6)
-baystars.calc_win_rate
 baystars.show_team_result
 
 carp = Team.new("Carp", 52, 56, 12)
-carp.calc_win_rate
 carp.show_team_result
 
 swallows = Team.new("Swallows", 41, 69, 10)
-swallows.calc_win_rate
 swallows.show_team_result
